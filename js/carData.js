8f621692728095abab61c1daec4bad3c1671b7fd
KSimmp <k_simpson22@fanshaweonline.ca>
2017-11-24 09:53:36 -0500

    finished car picker

--- js/carData.js
+++ js/carData.js
@@ -1,63 +1,25 @@
 var carData = {
-  f55: {
+  F55 : {
     car : "Mini Cooper F55",
 
     price : "$27,790",
 
-    description : "spring_1.jpg",
+    description : "It’s the turbocharged motoring icon, with more handles to love. The MINI 5 door boasts the same iconic good looks as its 3 door brother, but features an extra pair of passenger doors for practical access to its spacious cabin. And despite its slightly longer frame, the MINI 5 door’s wide, athletic stance and wheels-pushed-out design deliver the corner-carving agility you’d expect from any MINI.",
   },
 
-  summer : {
-    text : "Summer is the warmest of the four temperate seasons, falling between spring and autumn. At the summer solstice, the days are longest and the nights are shortest, with day-length decreasing as the season progresses after the solstice. The date of the beginning of summer varies according to climate, tradition and culture, but when it is summer in the Northern Hemisphere it is winter in the Southern Hemisphere, and vice versa.",
+  F56 : {
+    car : "Mini Cooper F56",
 
-    headline : "Summer",
+    price : "$22,190",
 
-    images : [
-      "summer_1.jpg",
-      "summer_2.jpg",
-      "summer_3.jpg"
-    ],
-
-    imageDescription : [
-      "Beach chairs",
-      "Dog sleeping in hammock",
-      "Sunflower field"
-    ]
+    description : "A true style icon – reimagined for contemporary living. Paying tribute to the original 1959 Austin Seven, the MINI Seven Edition brings together our most distinctive elements into one model. Striking Melting Silver details, a unique bonnet stripe design, and exclusive interior elements put this special edition MINI into a class of its own.",
   },
 
-  autumn : {
-    text : "Autumn, known as fall in the US and Canada,[1] is one of the four temperate seasons. Autumn marks the transition from summer into winter, in September (Northern Hemisphere) or March (Southern Hemisphere) when the arrival of night becomes noticeably earlier. One of its main features is the shedding of leaves from deciduous trees as they pave way for further growth. In North America, autumn is usually considered to start with the September equinox.[5] In traditional East Asian solar term, autumn starts on or around 8 August and ends on about 7 November. In Ireland, the autumn months according to the national meteorological service, Met Éireann, are September, October and November.[6] However, according to the Irish Calendar, which is based on ancient Gaelic traditions, autumn lasts throughout the months of August, September, and October, or possibly a few days later, depending on tradition. In Australia, autumn officially begins on 1 March and ends 31 May.[7]",
+  R58 : {
+    car : "Mini Cooper R58",
 
-    headline : "Autumn",
+    price : "$40,590",
 
-    images : [
-      "autumn_1.jpg",
-      "autumn_2.jpg",
-      "autumn_3.jpg"
-    ],
-
-    imageDescription : [
-      "Fallen trail",
-      "Pumpkins and sunflowers",
-      "Big orange tree - Original Content"
-    ]
+    description : "It’s almost time to re-discover the redesigned, re-engineered, and completely revamped MINI Convertible. From feistier more fuel-efficient engines, to smarter innovations that let you stay open at every point along the way, this all-new MINI Convertible brings an array of enhancements to get excited about. And although the next iteration of this sun-seeking MINI will look and feel more refined than its predecessor, the open-air exhilaration that awaits you behind the wheel remains the same. We can’t wait for you to experience it in person. Until then... stay open.",
   },
-
-  winter : {
-    text : "Winter (/ˈwɪntər/) is the coldest season of the year in temperate climates, between autumn and spring. It is caused by the axis of the Earth in the respective hemisphere being oriented away from the Sun. Different cultures define different dates as the start of winter, and some use a definition based on weather, but when it is winter in the Northern Hemisphere it is summer in the Southern Hemisphere, and vice versa. In many regions, winter is associated with snow and freezing temperatures. At the winter solstice, the days are shortest and the nights are longest, with days lengthening as the season progresses after the solstice.",
-
-    headline : "Winter",
-
-    images : [
-      "winter_1.jpg",
-      "winter_2.jpg",
-      "winter_3.jpg"
-    ],
-
-    imageDescription : [
-      "White forest",
-      "Snowman",
-      "Green-eyed wolf"
-    ]
-  }
 };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 (function () {
-  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-   
+  var thePics = document.querySelectorAll('.data-ref'),
+  theCarTitle = document.querySelector('.modelName'),
+  thePrice = document.querySelector('.priceInfo'),
+  theDesc = document.querySelector('.modelDetails'),
+  appliedClass;
 
+  function ChangeElements(){
+    let objectIndex = carData[this.id];
+    thePrice.firstChild.nodeValue = objectIndex.price;
+    theDesc.firstChild.nodeValue = objectIndex.description;
+    theCarTitle.firstChild.nodeValue = objectIndex.car;
+
+    appliedClass = this.id;
+  }
+
+  thePics.forEach(function(element, index){
+    //Loop through the images and add event handling to each one
+    element.addEventListener('click', ChangeElements, false);
+  })
+
+  ChangeElements.call(document.querySelector('#F55'));
 })();
